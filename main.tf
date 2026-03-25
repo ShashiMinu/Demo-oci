ec19b35f3c200b530a8a66c5e517794cd88f590a
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 11:01:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ terraform {
 resource "random_pet" "name" {
   length = 2
 }
+resource "oci_identity_compartment" "test_compartment" {
+    #Required
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaahwuksuenazr4vnx2kh2vepw3ealmcjkghp45iz33x4fw6s3ce2xq"
+    description = "for ci/cd testing"
+    name = "demo"
+
+    #Optional
+    defined_tags = {"Operations.CostCenter"= "42"}
+    freeform_tags = {"Department"= "Finance"}
+}
