a1c79c298ad2b4f065f731a294da92f06adbb73f
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 11:26:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,10 @@
+locals {
+  private_key_path = "oci_api_key.pem"
+}
 provider "oci" {
   region       = var.region
   tenancy_ocid = var.tenancy_ocid
   user_ocid    = var.user_ocid
   fingerprint  = var.fingerprint
-  private_key  = var.private_key
+  private_key  = locals.private_key_path
 }
