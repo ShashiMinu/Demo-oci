6cde4545c0eff0c5a81d1f7b5ef280064975c738
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 18:59:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ resource "oci_identity_compartment" "test_compartment" {
     #Required
     compartment_id = "ocid1.compartment.oc1..aaaaaaaahwuksuenazr4vnx2kh2vepw3ealmcjkghp45iz33x4fw6s3ce2xq"
     description = "for ci/cd testing"
-    name = "demo"
+    name = "ci_cd_comp"
 }
