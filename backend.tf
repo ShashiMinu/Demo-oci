ef8c5f2ac19620263923207a7145d22ecb35a15f
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 09:48:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket                      = "cribl-test-bucket"
     key                         = "demo/terraform.tfstate"
     region                      = "us-ashburn-1"
-    endpoint                    = {s3 ="https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"}
+    endpoint                    = "https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
