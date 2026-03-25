45e5f6e8130f7aa0235e75498210a6ca4c096e96
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 16:36:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,15 +1,15 @@
-#terraform {
- # backend "s3" {
-   # bucket                      = "cribl-test-bucket"
-   # key                         = "dev/terraform.tfstate"   # change per env
-    #region                      = "us-ashburn-1"
+terraform {
+  backend "s3" {
+    bucket                      = "cribl-test-bucket"
+    key                         = "dev/terraform.tfstate"   # change per env
+    region                      = "us-ashburn-1"
 
-   # endpoint                    = "https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"
-    #skip_credentials_validation = true
-    #skip_region_validation      = true
-   # skip_requesting_account_id  = true
-   # use_path_style              = true
-   # disable_chunked_encoding = true
+    endpoint                    = "https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    #disable_chunked_encoding = true
 
-#  }
-#}
+ }
+}
