4130c52312ebcded937a80974829857d3a7b0554
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 15:44:14 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket                      = "cribl-test-bucket"
+    key                         = "dev/terraform.tfstate"   # change per env
+    region                      = "us-ashburn-1"
+
+    endpoint                    = "https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+
+    access_key                  = var.user_ocid
+    secret_key                  = var.private_key
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
+}
