c80d44aadbd234749ad3493f8cc9459b47c42244
ShashiMinu <minumulashashikanth@gmail.com>
2026-03-25 16:01:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,10 +5,6 @@ terraform {
     region                      = "us-ashburn-1"
 
     endpoint                    = "https://idqjvvuiw1f5.compat.objectstorage.us-ashburn-1.oraclecloud.com"
-
-    access_key                  = ${{secrets.SECRET_KEY }}
-    secret_key                  = ${{secrets.ACCESS_KEY_ID }}
-
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
